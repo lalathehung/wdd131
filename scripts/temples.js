06c1bd4e0777ee1624bdb0f0b41f105109a272d0
lalathehung <1808arthur@gmail.com>
2025-03-24 22:59:18 -0300

    Updated

---
+++ scripts/temples.js
@@ -0,0 +1,17 @@
+document.addEventListener('DOMContentLoaded', () => {
+    const hamButton = document.querySelector('#menu');
+    const navigation = document.querySelector('.navigation');
+
+    hamButton.addEventListener('click', () => {
+        navigation.classList.toggle('open');
+        hamButton.classList.toggle('open');
+    });
+
+    // Footer information
+    const currentYear = new Date().getFullYear();
+    const lastModified = document.lastModified;
+    const copyrightYearElement = document.getElementById('currentyear');
+    const lastModifiedElement = document.getElementById('lastModified');
+    copyrightYearElement.textContent = currentYear;
+    lastModifiedElement.textContent = `Last update: ${lastModified}`;
+});
